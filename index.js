b1dca64ef4cc8be16feb270d4ae5f721c913417f
Masud Rana <masudrana799dev@gmail.com>
2023-02-21 17:26:30 +0600

    apply namespace

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 const express = require("express");
 const http = require("http");
+// const { createServer } = require("http");
 const { Server } = require("socket.io");
 const app = express();
-const PORT = 4000;
 
 // create server through http
 const httpServer = http.createServer(app);
 
 // create socket server based on httpServer
-const io = new Server(httpServer);
+const io = new Server(httpServer, { /* options */ });
 
-// socket Middlewares 
-// io.use((socket, next) => {
-//   const token = socket.handshake.auth.token;
-//   console.log(token);
-
-//   try{
-//       var decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-//       next();
-//   }catch(e){   
-//   }
-// });
 
 
 app.get("/", (req, res) => {
@@ -30,47 +19,32 @@ app.get("/", (req, res) => {
 
 
 
-// io.on(eventName, cbfn); // on => listening
+//io.on() Listen
 
-// on => Listening, emit => Speaking
-io.on("connection", (socket) => {
-  console.log("Connection is ready", socket.id);
+//io.emit() speak
+/* io.on("connection", (socket) => {
+    console.log("Client Connected ",socket.id)
+}); */
 
-  socket.on("disconnect", () =>{
-    console.log("Disconnected!", socket.id);
+io.of("/admin").on("connection", (socket) => {
+  console.log('Admin Connected ',socket.id);
+  socket.on('adminEvent',(payload)=>{
+      console.log('Payload',payload);
   });
-
-  socket.on('clientEvent', (payload) => {
-    console.log("Msg->", payload);
-
-    // =>  Speak to only socket (only sender)
-    // socket.emit('serverEvent', payload); 
-    
-    // => Speak to all except sender
-    socket.broadcast.emit('serverEvent', payload); 
-    
-    // => Speak to all
-    // io.emit('serverEvent', payload); 
-  });
-
-  // Room Example
-  socket.on("groupNameEvent", (arg) => {
-    // save room info permanently in db
-    let roomName = arg;
-    
-    // Join the Room
-    socket.join(roomName);
-    // => to all
-    // io.to(roomName).emit("OkRoomServerEvent", `Welcome to Room(${arg}). ${socket.id} joined`);
-
-    // => to all except sender 
-    socket.to(roomName).emit("OkRoomServerEvent", `Welcome to Room(${arg}). ${socket.id} joined`);
-
-  });
-
 });
 
+io.of("/teacher").on("connection", (socket) => {
+  console.log('Teacher Connected ',socket.id);
+});
 
+io.of("/student").on("connection", (socket) => {
+  console.log('Student Connected ',socket.id);
+  socket.on('studentEvent',(payload)=>{
+      console.log('Payload',payload);
+  });
+});
+
+const PORT =  process.env.PORT || 4000;
 httpServer.listen(PORT, () => {
-  console.log("Server is running at http://localhost:4000");
+  console.log(`Server is running at ${PORT}`);
 });
