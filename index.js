34642749138cf0faa9a305a6ffbf2f80d532a9de
Masud Rana <masudrana799dev@gmail.com>
2023-02-21 17:06:18 +0600

    add room feature

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const httpServer = http.createServer(app);
 // create socket server based on httpServer
 const io = new Server(httpServer);
 
+// socket Middlewares 
+// io.use((socket, next) => {
+//   const token = socket.handshake.auth.token;
+//   console.log(token);
+
+//   try{
+//       var decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+//       next();
+//   }catch(e){   
+//   }
+// });
+
+
 app.get("/", (req, res) => {
   // res.json({ data: "hello world from socket" });
   res.sendFile(__dirname + "/client/index.html");
@@ -17,7 +30,7 @@ app.get("/", (req, res) => {
 
 
 
-// io.on(eventName, cbfn); // on => listen
+// io.on(eventName, cbfn); // on => listening
 
 // on => Listening, emit => Speaking
 io.on("connection", (socket) => {
@@ -30,9 +43,30 @@ io.on("connection", (socket) => {
   socket.on('clientEvent', (payload) => {
     console.log("Msg->", payload);
 
-    socket.emit('serverEvent', payload); // Speak to socket
-    // io.emit('serverEvent', payload); // Speak to all
-  })
+    // =>  Speak to only socket (only sender)
+    // socket.emit('serverEvent', payload); 
+    
+    // => Speak to all except sender
+    socket.broadcast.emit('serverEvent', payload); 
+    
+    // => Speak to all
+    // io.emit('serverEvent', payload); 
+  });
+
+  // Room Example
+  socket.on("groupNameEvent", (arg) => {
+    // save room info permanently in db
+    let roomName = arg;
+    
+    // Join the Room
+    socket.join(roomName);
+    // => to all
+    // io.to(roomName).emit("OkRoomServerEvent", `Welcome to Room(${arg}). ${socket.id} joined`);
+
+    // => to all except sender 
+    socket.to(roomName).emit("OkRoomServerEvent", `Welcome to Room(${arg}). ${socket.id} joined`);
+
+  });
 
 });
 
