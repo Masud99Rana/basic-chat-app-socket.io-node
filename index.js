775846c59262ff8519e63bf820ef29d11babb9b5
Masud Rana <masudrana799dev@gmail.com>
2023-02-21 13:37:43 +0600

    add converstation feature, date utils

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const app = express();
 const PORT = 4000;
 
+// create server through http
 const httpServer = http.createServer(app);
-const io = new Server(httpServer);
 
-import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// create socket server based on httpServer
+const io = new Server(httpServer);
 
 app.get("/", (req, res) => {
   // res.json({ data: "hello world from socket" });
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/client/index.html");
 });
 
+
+
+// io.on(eventName, cbfn); // on => listen
+
+// on => Listening, emit => Speaking
 io.on("connection", (socket) => {
-  console.log("Connection is ready");
+  console.log("Connection is ready", socket.id);
 
-  socket.on("disconnect", (socket) =>{
-    console.log("Disconnected!");
+  socket.on("disconnect", () =>{
+    console.log("Disconnected!", socket.id);
   });
+
+  socket.on('clientEvent', (payload) => {
+    console.log("Msg->", payload);
+
+    socket.emit('serverEvent', payload); // Speak to socket
+    // io.emit('serverEvent', payload); // Speak to all
+  })
+
 });
-
-
 
 
 httpServer.listen(PORT, () => {
